3553801c34c86ca16593533a286dcb81f8b26bde
Divij Rana <131740630+Himeshdivij@users.noreply.github.com>
2025-06-04 09:42:44 +0530

    Create BooksRepository.cs

---
+++ DAY-4/Books.DataAccess/Repositories/BooksRepository.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Books.DataAccess.Models;
+
+namespace Books.DataAccess.Repositories
+{
+    public class BooksRepository
+    {
+        private readonly BooksDbContext _bookDbContext;
+
+        public BooksRepository(BooksDbContext booksDbContext)
+        {
+            _bookDbContext = booksDbContext;
+        }
+
+      
+        public Book GetBookById(int id)
+        {
+            Book book = _bookDbContext.Books.FirstOrDefault(book => book.Id == id);
+            if (book == null)
+            {
+                return null;
+            }
+            return book;
+        }
+
+        public void AddBook(Book book)
+        {
+            _bookDbContext.Books.Add(book);
+            _bookDbContext.SaveChanges();
+        }
+
+        public int UpdateBook(Book book)
+        {
+            Book bookToBeUpdated = GetBookById(book.Id);
+            if (bookToBeUpdated == null)
+            {
+                return -1;
+            }
+            else
+            {
+                bookToBeUpdated.Title = book.Title;
+                bookToBeUpdated.Author = book.Author;
+                bookToBeUpdated.Genre = book.Genre;
+                _bookDbContext.SaveChanges();
+                return 1;
+            }
+        }
+
+        public int DeleteBook(int id)
+        {
+            Book bookToBeRemoved = GetBookById(id);
+            if (bookToBeRemoved == null)
+            {
+                return -1;
+            }
+            else
+            {
+                _bookDbContext.Books.Remove(bookToBeRemoved);
+                _bookDbContext.SaveChanges();
+                return 1;
+            }
+        }
+
+        
+    }
+}
