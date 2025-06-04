45921498dbbeba05d946c6f03ac9ff5286702de0
Divij Rana <131740630+Himeshdivij@users.noreply.github.com>
2025-06-04 09:36:44 +0530

    Add files via upload

---
+++ DAY-4/Book_Management/Program.cs
@@ -0,0 +1,49 @@
+using Books.DataAccess;
+using Books.DataAccess.Repositories;
+using Books.Services.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Management
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Add DbContext with explicit MigrationsAssembly
+            builder.Services.AddDbContext<BooksDbContext>(options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    npgsqlOptions => npgsqlOptions.MigrationsAssembly("Books.DataAccess")
+                )
+            );
+
+            builder.Services.AddScoped<BooksService>();
+            builder.Services.AddScoped<BooksRepository>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
